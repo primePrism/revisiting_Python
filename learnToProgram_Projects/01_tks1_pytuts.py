b84dc656da040ac5de45b16cc82d000afa74d721
primePrism <104704979+primePrism@users.noreply.github.com>
2023-08-02 01:12:44 +0000

    basic operation and type conversion done

---
+++ learnToProgram_Projects/01_tks1_pytuts.py
@@ -0,0 +1,11 @@
+# Problem : Receive miles and convert to kilometers
+# Kilometers = miles * 1.60934
+
+
+miles = input("Input a distance in miles: ")
+
+miles = float(miles)
+
+kilometers = miles * 1.60934
+
+print("{}miles is equivalent to {}km".format(miles, kilometers))
