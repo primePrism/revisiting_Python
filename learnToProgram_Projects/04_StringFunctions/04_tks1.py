3c73c2ae6715dc5f47efe50d16e19076a7fa3953
primePrism <104704979+primePrism@users.noreply.github.com>
2023-08-09 21:12:27 +0000

    updated task 1 of 4

--- learnToProgram_Projects/04_StringFunctions/04_tks1.py
+++ learnToProgram_Projects/04_StringFunctions/04_tks1.py
@@ -1,2 +1,12 @@
 # _______________ Acronym Generator_________________
 
+input_String = input("Please enter your phrase: ")
+
+input_String = input_String.upper()
+
+listWords = input_String.split()
+
+for words in listWords:
+    print(words[0], end="")
+
+print()
