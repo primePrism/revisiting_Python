9eb672353cfd84d944aef73ead23bfe18fb23264
primePrism <104704979+primePrism@users.noreply.github.com>
2023-08-10 23:58:14 +0000

    removed comments

--- learnToProgram_Projects/05_Funtions/05_pytuts.py
+++ learnToProgram_Projects/05_Funtions/05_pytuts.py
@@ -8,24 +8,18 @@ def ceasorCypher(message):
 
     secretMessage = ""
 
-    # Cycle through each character in the message
     for char in message:
-        # If it isn't a letter then keep it as it is in the else below
         if char.isalpha():
-            # Get the character code and add the shift amount
             charCode = ord(char)
             charCode += encryptKey
 
-            # If uppercase then compare to uppercase unicodes
             if char.isupper():
-                # If bigger than Z subtract 26
                 if charCode > ord("Z"):
                     charCode -= 26
 
-                # If smaller than A add 26
                 if charCode < ord("A"):
                     charCode += 26
-            # Do the same for lowercase characters
+
             else:
                 if charCode > ord("z"):
                     charCode -= 26
@@ -33,10 +27,8 @@ def ceasorCypher(message):
                 if charCode < ord("a"):
                     charCode += 26
 
-            # Convert from code to letter and add to message
             secretMessage += chr(charCode)
 
-        # If not a letter leave the character as is
         else:
             secretMessage += char
 
