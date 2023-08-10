fbbb108ad70b20fb4e836ed7ab7165e8a799e334
primePrism <104704979+primePrism@users.noreply.github.com>
2023-08-10 23:40:37 +0000

    added decryption

--- learnToProgram_Projects/04_StringFunctions/04_tks2.py
+++ learnToProgram_Projects/04_StringFunctions/04_tks2.py
@@ -46,3 +46,41 @@ for char in message:
         secretMessage += char
 
 print("Encrypted message: ", secretMessage)
+
+# To decrypt the only thing that changes is the sign of the key
+DecryptedMessage = ""
+encryptKey = -encryptKey
+
+
+for char in secretMessage:
+    # If it isn't a letter then keep it as it is in the else below
+    if char.isalpha():
+        # Get the character code and add the shift amount
+        charCode = ord(char)
+        charCode += encryptKey
+
+        # If uppercase then compare to uppercase unicodes
+        if char.isupper():
+            # If bigger than Z subtract 26
+            if charCode > ord("Z"):
+                charCode -= 26
+
+            # If smaller than A add 26
+            if charCode < ord("A"):
+                charCode += 26
+        # Do the same for lowercase characters
+        else:
+            if charCode > ord("z"):
+                charCode -= 26
+
+            if charCode < ord("a"):
+                charCode += 26
+
+        # Convert from code to letter and add to message
+        DecryptedMessage += chr(charCode)
+
+    # If not a letter leave the character as is
+    else:
+        DecryptedMessage += char
+
+print("Decrypted message: ", DecryptedMessage)
