4b052b53ef7c1d0e9c1e5de02f9baea1eb213fc0
primePrism <104704979+primePrism@users.noreply.github.com>
2023-08-10 23:35:12 +0000

    added simple ecryption

--- learnToProgram_Projects/04_StringFunctions/04_tks2.py
+++ learnToProgram_Projects/04_StringFunctions/04_tks2.py
@@ -7,26 +7,42 @@
 
 
 # Receive the message to encrypt and the number of characters to shift
+message = input("Enter your message to be encrypted: ")
+encryptKey = int(input("Enter a number between 0 - 27 to serve as an encryption key: "))
 
 # Prepare your secret message
+secretMessage = ""
 
 # Cycle through each character in the message
-
+for char in message:
     # If it isn't a letter then keep it as it is in the else below
-
+    if char.isalpha():
         # Get the character code and add the shift amount
+        charCode = ord(char)
+        charCode += encryptKey
 
         # If uppercase then compare to uppercase unicodes
-
+        if char.isupper():
             # If bigger than Z subtract 26
-            
+            if charCode > ord("Z"):
+                charCode -= 26
+
             # If smaller than A add 26
-            
+            if charCode < ord("A"):
+                charCode += 26
         # Do the same for lowercase characters
+        else:
+            if charCode > ord("z"):
+                charCode -= 26
+
+            if charCode < ord("a"):
+                charCode += 26
 
         # Convert from code to letter and add to message
+        secretMessage += chr(charCode)
 
     # If not a letter leave the character as is
+    else:
+        secretMessage += char
 
-
-# To decrypt the only thing that changes is the sign of the key
+print("Encrypted message: ", secretMessage)
