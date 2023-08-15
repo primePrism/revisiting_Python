1b7d81fe430cf4a480f6fc4722f45210e0404e3f
primePrism <104704979+primePrism@users.noreply.github.com>
2023-08-15 21:15:44 +0000

    added splater operator
    q

---
+++ learnToProgram_Projects/05_Funtions/05_tks1.py
@@ -0,0 +1,14 @@
+# Splater Operator
+# for functions with unknown number of arguments
+
+
+def sumAll(*args):
+    sum = 0
+
+    for i in args:
+        sum += i
+
+    return sum
+
+
+print("Summing everything is : ", sumAll(1, 54, 89))
