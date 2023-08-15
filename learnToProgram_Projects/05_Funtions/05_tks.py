5eec0df8d5da6ccd1ff24b6102dcc413e9b7bde1
primePrism <104704979+primePrism@users.noreply.github.com>
2023-08-15 21:04:41 +0000

    added task 5

--- learnToProgram_Projects/05_Funtions/05_tks.py
+++ learnToProgram_Projects/05_Funtions/05_tks.py
@@ -8,3 +8,29 @@ def solve_X(equation):
     num1, num2 = int(num1), int(num2)
 
     return "x = " + str(num2 - num1)
+
+
+def isPrime(num):
+    for x in range(2, num):
+        if (num % x) == 0:
+            return False
+
+    return True
+
+
+def getPrimes(maxNumber):
+    list_of_primes = []
+
+    for num1 in range(2, maxNumber):
+        if isPrime(num1):
+            list_of_primes.append(num1)
+
+    return list_of_primes
+
+
+max_num_to_check = int(input("Search for primes up to: "))
+
+list_of_primes = getPrimes(max_num_to_check)
+
+for prime in list_of_primes:
+    print(prime)
